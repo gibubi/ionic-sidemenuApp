6c761ece9315bf8fcd1becb1c9fd2463310e5fdf
gibubi <gibubi@daum.net>
2018-05-31 21:14:32 +0900

    add http

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import {ModalController, NavController} from 'ionic-angular';
+import {AlertController, ModalController, NavController} from 'ionic-angular';
 import {Profile} from "../../interfaces/profile";
+import {Account} from "../../interfaces/account";
 
 @Component({
   selector: 'page-home',
@@ -9,9 +10,12 @@ import {Profile} from "../../interfaces/profile";
 export class HomePage {
 
   public profile = {} as Profile;
+  public accountData = {} as Account;
 
   constructor(public navCtrl: NavController,
-              public modalCtrl : ModalController) {
+              public modalCtrl : ModalController,
+              public alertCtrl : AlertController
+              ) {
 
   }
 
@@ -35,5 +39,43 @@ export class HomePage {
       }
     });
     modal.present();
+  }
+
+  //Prompt Alert
+  showPrompt() {
+    let prompt = this.alertCtrl.create({
+      title: 'Login',
+      message: "이름과 이메일을 입력하세요",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: '이름 입력'
+        },
+        {
+          name: 'email',
+          placeholder: '이메일 입력'
+        },
+      ],
+      buttons: [
+        {
+          text: '취소',
+          handler: data => {
+            console.log('Cancel clicked');
+            console.log(data);
+          }
+        },
+        {
+          text: '저장',
+          handler: data => {
+            console.log('Saved clicked');
+            console.log(data);
+
+            this.accountData = {name:data.name, email:data.email};
+            this.navCtrl.push('NavPage', {account:this.accountData});
+          }
+        }
+      ]
+    });
+    prompt.present();
   }
 }
